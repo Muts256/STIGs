118a00dd1bad044f48d89053e9a1e1bd5758908c
Michael Musoke <msokey678@outlook.com>
2025-12-25 19:42:08 +0000

    Update WN10-AU-000585.ps1

--- WN10-AU-000585.ps1
+++ WN10-AU-000585.ps1
@@ -10,7 +10,7 @@
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
-    STIG-ID         : WN10-CC-000230
+    STIG-ID         : WN10-AU-000585
 
 .TESTED ON
     Date(s) Tested  : 
@@ -21,7 +21,7 @@
 .USAGE
     Put any usage instructions here.
     Example syntax:
-    PS C:\> .\__remediation_template(STIG ID: WN10-CC-000230).ps1 
+    PS C:\> .\__remediation_template(STIG ID: WN10-AU-000585).ps1 
 #>
 # ============================================================
 # STIG ID: WN10-AU-000585
